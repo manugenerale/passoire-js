97577fd739c5c2edb52dee0910b4397b0984ed68
Pierre-Emmanuel Pourquier <pepourquier@medianetroom.com>
2015-10-09 18:33:29 +0200

    Add click function

--- assets/passoire.js
+++ assets/passoire.js
@@ -1,5 +1,14 @@
 function passoire() {
-	$('body').css('background', 'url("../image/passoire.png") center center repeat');	
+	$('body').css('background', 'url("../img/passoire.svg") center center repeat');	
 	$('body').css('background-size', 'cover');	
+	$('body').attr('data-situation', 'passoire');	
 };
+$('#passoire').click(function() {
+    if($('body').attr('data-situation') == 'passoire') {
+	    $('body').css('background', 'none');	 
+	    $('body').attr('data-situation', 'clair');	 
+    } else {
+	     passoire();
+    }
+});
 
